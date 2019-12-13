139e0da2f48d4d7be5afe2347f6bb42cb9e96ed1
VToreZ <lumisitone@gmail.com>
2019-12-13 14:28:48 +0300

    fix for travis

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     if (argc < 2)
     {
-        path path_to_ftp("/home/rooted/Documents/laba4/ftp");
+        path path_to_ftp("/home/rooted/Documents/lab-04-boost-filesystem/ftp");
         iterate(path_to_ftp);
     } else {
         path path_to_ftp(argv[1]);

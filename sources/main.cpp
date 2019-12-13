6de4aa2d2196c450383f4f18dde51e8c36c0442c
VToreZ <lumisitone@gmail.com>
2019-12-13 14:23:39 +0300

    prefinal

---
+++ sources/main.cpp
@@ -0,0 +1,15 @@
+//Copyright 2019 <VToreZ>
+#include "FTPfiles.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2)
+    {
+        path path_to_ftp("/home/rooted/Documents/laba4/ftp");
+        iterate(path_to_ftp);
+    } else {
+        path path_to_ftp(argv[1]);
+        iterate(path_to_ftp);
+    }
+
+    return 0;
+}
